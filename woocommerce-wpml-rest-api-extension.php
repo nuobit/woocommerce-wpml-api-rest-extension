e469d37c2130227cf363eeefa54595c967e15d5e
Eric Antones <eantones@nuobit.com>
2025-12-16 17:30:28 +0100

    [FIX] Wrong repo name

--- woocommerce-wpml-rest-api-extension.php
+++ woocommerce-wpml-rest-api-extension.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name: WooCommerce WPML REST API Extension
- * Plugin URI: https://github.com/nuobit/woocomerce-wpml-api-rest-extension
+ * Plugin URI: https://github.com/nuobit/woocommerce-wpml-api-rest-extension
  * Description: Extends WPML’s REST API integration by adding missing multilingual support, enhancing compatibility, and providing fixes for WooCommerce and other REST endpoints.
  * Version: 1.0.1
  * Author: NuoBiT Solutions, S.L.
  * Author URI: https://www.nuobit.com/
  * License: GPLv3 or later
- * Text Domain: woocomerce-wpml-api-rest-extension
+ * Text Domain: woocommerce-wpml-api-rest-extension
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html 
  */
 
